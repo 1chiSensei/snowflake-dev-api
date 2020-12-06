3f3e0d64367a7599ed425dfe3456c56862ae647b
1chi <Nyes3704@gmail.com>
2020-12-06 00:27:21 +0000

    added typings

---
+++ typings/index.d.ts
@@ -0,0 +1,11 @@
+export function Snowflake(...args: any[]): void;
+
+export namespace Snowflake {
+	function cat(): Promise<Buffer>
+
+	function dog(): Promise<Buffer>
+
+	function fox(): Promise<Buffer>
+
+	function meme(subreddit?: string): Promise<object>
+}
